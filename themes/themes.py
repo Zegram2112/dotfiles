01d6bd88d7ed601cd684d696c328b76234491e99
Nicolás Lemuñir <nicolas.lemunir2112@gmail.com>
2019-07-29 00:41:15 -0400

    Add themes system for polybar and kitty

---
+++ themes/themes.py
@@ -0,0 +1,35 @@
+import sys
+
+import generators as gens
+import os
+
+def refreshPolybar():
+    os.system("wmname bspwm")
+    os.system("killall polybar")
+    os.system("al-polybar-session")
+    os.system("wmname LG3D")
+
+def refreshKitty(kittyGenerator):
+    os.system(f"kitty @ set-colors --all {kittyGenerator.file_out}")
+
+def refresh(gen_instances):
+    refreshPolybar()
+    refreshKitty(gen_instances["kitty"])
+    os.system("rcup")
+
+def main():
+    if len(sys.argv) != 2:
+        return
+    gen_classes = {
+        "polybar": gens.PolybarColorGenerator,
+        "kitty": gens.KittyColorGenerator,
+    }
+    gen_instances = {}
+    for key, gen_class in gen_classes.items():
+        gen = gen_class(sys.argv[1])
+        gen_instances[key] = gen
+        gen.generate()
+    refresh(gen_instances)
+
+main()
+
