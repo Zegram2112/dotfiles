4e56b5559a6cf476b745c33abd0be04fd865f213
Nicolás Lemuñir <nicolas.lemunir2112@gmail.com>
2019-08-05 11:43:28 -0400

    Update themes.py

--- themes/themes.py
+++ themes/themes.py
@@ -3,18 +3,18 @@ import sys
 import generators as gens
 import os
 
-def refreshPolybar():
+def refresh_polybar():
     os.system("wmname bspwm")
     os.system("killall polybar")
     os.system("al-polybar-session -z ~/.config/polybar/sessions/custom-sessionfile")
     os.system("wmname LG3D")
 
-def refreshKitty(kittyGenerator):
-    os.system(f"kitty @ set-colors --all {kittyGenerator.file_out}")
+def refresh_kitty(kitty_generator):
+    os.system(f"kitty @ set-colors -a -c {kitty_generator.file_out}")
 
 def refresh(gen_instances):
-    refreshPolybar()
-    refreshKitty(gen_instances["kitty"])
+    refresh_polybar()
+    refresh_kitty(gen_instances["kitty"])
     os.system("rcup")
 
 def main():
